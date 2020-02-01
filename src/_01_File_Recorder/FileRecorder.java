03672b8ebdcf0302ab532567d7e8cf5de898d5ba
League Student <league@iMac-12.attlocal.net>
2020-02-01 15:59:51 -0800

    File Encryption and Decryption
    Finish this and figure out error with Null output

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,62 @@
 package _01_File_Recorder;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	
+	public static void main(String[] args) {
+		FileRecorder recorder = new FileRecorder();
+		
+	}
+	
+	FileRecorder(){
+		
+		
+		
+		
+	}
+	void recorder(String message) {
+		try {
+			FileWriter writer = new FileWriter("src/_01_File_Recorder/file");
+			
+			writer.append(message);
+			writer.close();
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	static File createFile(String message) {
+		File file;
+		if(message.isEmpty()) {
+			file = null;
+			return file;
+			
+		}
+		if(message.contains(" ") && message.indexOf(" ") < 10) {
+			file = new File(message.substring(0, message.indexOf(" ")) + ".txt", message);
+			
+		}else if(message.length() > 10){
+			file = new File(message.substring(0, 10) + ".txt", message);
+			
+		}else {
+			file = new File(message + ".txt", message);
+			
+		}
+		
+		return file;
+		
+		
+	}
+	
+	
 }
--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -1,7 +1,61 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileEncryptor {
 	// Create a program that takes a message from the user.
 	// Use the methods in the String and Character classes to save
 	// an encrypted form of the message to a file
+	
+	public static void main(String[] args) {
+		FileEncryptor e = new FileEncryptor();
+		
+		
+	}
+	
+	FileEncryptor(){
+		record(encrypt("This is a message"));
+		
+	}
+	
+	String encrypt(String message) {
+		String code = "";
+		
+		String[] letters = new String[message.length()];
+		for(int i = 0; i < message.length(); i ++) {
+			letters[i] = message.substring(i, i++);
+		}
+		
+		for(int i = 0; i < letters.length -1; i += 2) {
+			String flip = letters[i];
+			letters[i] = letters[i+1];
+			letters[i] = flip;
+			
+		}
+		
+		for(int i = 0; i < letters.length; i ++) {
+			code = code + letters[i];
+			
+		}
+		System.out.println(code);
+		return code;
+	}
+	
+	void record(String message) {
+		try {
+			FileWriter writer = new FileWriter("src/_02_File_Encrypt_Decrypt/file");
+			
+			writer.append(message);
+			writer.close();
+			
+		} catch(IOException e)	{ 
+			
+			
+		}
+		
+	}
+
+
 }
